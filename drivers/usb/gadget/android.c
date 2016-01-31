2c29f7a99c20907067c8cd374be5bea7a86aa3b3
James Christopher Adduono <jc@adduono.com>
2016-01-31 04:08:53 -0500

    MIDI gadget support

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -31,6 +31,9 @@
 #include "gadget_chips.h"
 
 #include "f_fs.c"
+#ifdef CONFIG_SND_RAWMIDI
+#include "f_midi.c"
+#endif
 #include "f_audio_source.c"
 #include "f_mass_storage.c"
 #include "f_adb.c"
@@ -59,6 +62,12 @@ static int composite_string_index;
 /* Default vendor and product IDs, overridden by userspace */
 #define VENDOR_ID		0x18D1
 #define PRODUCT_ID		0x0001
+
+/* f_midi configuration */
+#define MIDI_INPUT_PORTS    1
+#define MIDI_OUTPUT_PORTS   1
+#define MIDI_BUFFER_SIZE    1024
+#define MIDI_QUEUE_LENGTH   32
 
 #ifdef CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
 /* DM_PORT NUM : /dev/ttyGS* port number */
@@ -246,6 +255,62 @@ static void android_disable(struct android_dev *dev)
 		usb_remove_config(cdev, &android_config_driver);
 	}
 }
+
+#ifdef CONFIG_SND_RAWMIDI
+static int midi_function_init(struct android_usb_function *f,
+					struct usb_composite_dev *cdev)
+{
+	struct midi_alsa_config *config;
+
+	config = kzalloc(sizeof(struct midi_alsa_config), GFP_KERNEL);
+	f->config = config;
+	if (!config)
+		return -ENOMEM;
+	config->card = -1;
+	config->device = -1;
+	return 0;
+}
+
+static void midi_function_cleanup(struct android_usb_function *f)
+{
+	kfree(f->config);
+}
+
+static int midi_function_bind_config(struct android_usb_function *f,
+						struct usb_configuration *c)
+{
+	struct midi_alsa_config *config = f->config;
+
+	return f_midi_bind_config(c, SNDRV_DEFAULT_IDX1, SNDRV_DEFAULT_STR1,
+			MIDI_INPUT_PORTS, MIDI_OUTPUT_PORTS, MIDI_BUFFER_SIZE,
+			MIDI_QUEUE_LENGTH, config);
+}
+
+static ssize_t midi_alsa_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct android_usb_function *f = dev_get_drvdata(dev);
+	struct midi_alsa_config *config = f->config;
+
+	/* print ALSA card and device numbers */
+	return sprintf(buf, "%d %d\n", config->card, config->device);
+}
+
+static DEVICE_ATTR(alsa, S_IRUGO, midi_alsa_show, NULL);
+
+static struct device_attribute *midi_function_attributes[] = {
+	&dev_attr_alsa,
+	NULL
+};
+
+static struct android_usb_function midi_function = {
+	.name		= "midi",
+	.init		= midi_function_init,
+	.cleanup	= midi_function_cleanup,
+	.bind_config	= midi_function_bind_config,
+	.attributes	= midi_function_attributes,
+};
+#endif
 
 /*-------------------------------------------------------------------------*/
 /* Supported functions initialization */
@@ -1542,6 +1607,9 @@ static struct android_usb_function *supported_functions[] = {
 #endif
 #ifdef CONFIG_USB_QCOM_MDM_BRIDGE
 	&qacm_function,
+#endif
+#ifdef CONFIG_SND_RAWMIDI
+	&midi_function,
 #endif
 	NULL
 };
