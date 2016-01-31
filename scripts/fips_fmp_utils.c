bffac7fe5241b587d9f8820184a5e5f93436b5b6
James Christopher Adduono <jc@adduono.com>
2016-01-31 02:46:29 -0500

    Really Samsung?

--- scripts/fips_fmp_utils.c
+++ scripts/fips_fmp_utils.c
@@ -9,76 +9,67 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv)
+#define SHA256_DIGEST_SIZE 32
+
+/*
+ * Given a vmlinux file, overwrites bytes at given offset with hmac bytes, available in
+ * hmac file.
+ * Return 0, if Success
+ *       -1, if Error
+ */
+int update_fmp_hmac(const char *vmlinux_path, const char *hmac_path, unsigned long offset)
 {
-	if (argc < 2) {
-		printf("\nUsage : \n");
-		printf("fips_fmp_utils -u vmlinux_file hmac_file offset");
-		printf("fips_fmp_utils -g vmlinux_file section_name offset size out_file");
-		printf("\n");
+	FILE *vmlinux_fp = NULL;
+	FILE *hmac_fp = NULL;
+	int i = 0, j = 0;
+	unsigned char hmac[SHA256_DIGEST_SIZE];
 
+	if (!vmlinux_path || !hmac_path || !offset) {
+		printf("FIPS update_fmp_hmac : Invalid Params");
 		return -1;
 	}
 
-	if (!strcmp("-u", argv[1])) {
-		unsigned long offset = 0;
-		unsigned char *vmlinux_file = NULL;
-		unsigned char *hmac_file = NULL;
-
-		if (argc != 5) {
-			printf("\nUsage : \n");
-			printf("fips_fmp_utils -u vmlinux_file hmac_file offset");
-			printf("\n");
-
-			return -1;
-		}
-
-		vmlinux_file = argv[2];
-		hmac_file = argv[3];
-		offset = atol(argv[4]);
-
-		if (!vmlinux_file || !hmac_file || !offset) {
-			printf ("./fips_fmp_utils -u vmlinux_file hmac_file offset");
-			return -1;
-		}
-
-		return update_fmp_hmac(vmlinux_file, hmac_file, offset);
-	} else if (!strcmp("-g", argv[1])) {
-		const char *in_file = NULL;
-		const char *section_name = NULL;
-		unsigned long offset = 0;
-		unsigned long size = 0;
-		const char *out_file = NULL;
-
-		if (argc != 7) {
-			printf("\nUsage : \n");
-			printf("./fips_fmp_utils -g vmlinux_file section_name offset size out_file");
-			printf("\n");
-
-			return -1;
-		}
-
-		in_file = argv[2];
-		section_name = argv[3];
-		offset = atol(argv[4]);
-		size = atol(argv[5]);
-		out_file = argv[6];
-
-		if (!in_file || !section_name || !offset || !size || !out_file) {
-			printf("./fips_fmp_utils -g vmlinux_file section_name offset size out_file");
-			return -1;
-		}
-
-		return collect_fmp_bytes(in_file, section_name, offset, size, out_file);
-	} else {
-		printf("\nUsage : \n");
-		printf("fips_fmp_utils -u vmlinux_file hmac_file offset");
-		printf("fips_fmp_utils -g vmlinux_file section_name offset size out_file");
-		printf("\n");
+	vmlinux_fp = fopen(vmlinux_path, "r+b");
+	if (!vmlinux_fp) {
+		printf("Unable to open vmlinux file ");
+		return -1;
 	}
 
-	return -1;
+	hmac_fp = fopen(hmac_path, "rb");
+
+	if (!hmac_fp) {
+		printf("Unable to open hmac file ");
+		fclose(vmlinux_fp);
+		return -1;
+	}
+
+	if (SHA256_DIGEST_SIZE != fread(&hmac, sizeof(unsigned char), SHA256_DIGEST_SIZE, hmac_fp)) {
+		printf("Unable to read %d bytes from hmac file", SHA256_DIGEST_SIZE);
+		fclose(hmac_fp);
+		fclose(vmlinux_fp);
+		return -1;
+	}
+
+	if (fseek(vmlinux_fp, offset, SEEK_SET) != 0) {
+		printf("Unable to seek into vmlinux file.");
+		fclose(hmac_fp);
+		fclose(vmlinux_fp);
+		return -1;
+	}
+
+	if (SHA256_DIGEST_SIZE !=  fwrite (hmac, sizeof(unsigned char), SHA256_DIGEST_SIZE, vmlinux_fp)) {
+		printf("Unable to write %d byte into vmlinux", SHA256_DIGEST_SIZE);
+		fclose(hmac_fp);
+		fclose(vmlinux_fp);
+		return -1;
+	}
+
+	fclose(vmlinux_fp);
+	fclose(hmac_fp);
+
+	return 0;
 }
 
 /*
@@ -155,64 +146,73 @@ int collect_fmp_bytes(const char *in_file, const char *section_name, unsigned lo
 	return 0;
 }
 
-#define SHA256_DIGEST_SIZE 32
-
-/*
- * Given a vmlinux file, overwrites bytes at given offset with hmac bytes, available in
- * hmac file.
- * Return 0, if Success
- *       -1, if Error
- */
-int update_fmp_hmac(const char *vmlinux_path, const char *hmac_path, unsigned long offset)
+int main(int argc, char **argv)
 {
-	FILE *vmlinux_fp = NULL;
-	FILE *hmac_fp = NULL;
-	int i = 0, j = 0;
-	unsigned char hmac[SHA256_DIGEST_SIZE];
+	if (argc < 2) {
+		printf("\nUsage : \n");
+		printf("fips_fmp_utils -u vmlinux_file hmac_file offset");
+		printf("fips_fmp_utils -g vmlinux_file section_name offset size out_file");
+		printf("\n");
 
-	if (!vmlinux_path || !hmac_path || !offset) {
-		printf("FIPS update_fmp_hmac : Invalid Params");
 		return -1;
 	}
 
-	vmlinux_fp = fopen(vmlinux_path, "r+b");
-	if (!vmlinux_fp) {
-		printf("Unable to open vmlinux file ");
-		return -1;
+	if (!strcmp("-u", argv[1])) {
+		unsigned long offset = 0;
+		unsigned char *vmlinux_file = NULL;
+		unsigned char *hmac_file = NULL;
+
+		if (argc != 5) {
+			printf("\nUsage : \n");
+			printf("fips_fmp_utils -u vmlinux_file hmac_file offset");
+			printf("\n");
+
+			return -1;
+		}
+
+		vmlinux_file = argv[2];
+		hmac_file = argv[3];
+		offset = atol(argv[4]);
+
+		if (!vmlinux_file || !hmac_file || !offset) {
+			printf ("./fips_fmp_utils -u vmlinux_file hmac_file offset");
+			return -1;
+		}
+
+		return update_fmp_hmac(vmlinux_file, hmac_file, offset);
+	} else if (!strcmp("-g", argv[1])) {
+		const char *in_file = NULL;
+		const char *section_name = NULL;
+		unsigned long offset = 0;
+		unsigned long size = 0;
+		const char *out_file = NULL;
+
+		if (argc != 7) {
+			printf("\nUsage : \n");
+			printf("./fips_fmp_utils -g vmlinux_file section_name offset size out_file");
+			printf("\n");
+
+			return -1;
+		}
+
+		in_file = argv[2];
+		section_name = argv[3];
+		offset = atol(argv[4]);
+		size = atol(argv[5]);
+		out_file = argv[6];
+
+		if (!in_file || !section_name || !offset || !size || !out_file) {
+			printf("./fips_fmp_utils -g vmlinux_file section_name offset size out_file");
+			return -1;
+		}
+
+		return collect_fmp_bytes(in_file, section_name, offset, size, out_file);
+	} else {
+		printf("\nUsage : \n");
+		printf("fips_fmp_utils -u vmlinux_file hmac_file offset");
+		printf("fips_fmp_utils -g vmlinux_file section_name offset size out_file");
+		printf("\n");
 	}
 
-	hmac_fp = fopen(hmac_path, "rb");
-
-	if (!hmac_fp) {
-		printf("Unable to open hmac file ");
-		fclose(vmlinux_fp);
-		return -1;
-	}
-
-	if (SHA256_DIGEST_SIZE != fread(&hmac, sizeof(unsigned char), SHA256_DIGEST_SIZE, hmac_fp)) {
-		printf("Unable to read %d bytes from hmac file", SHA256_DIGEST_SIZE);
-		fclose(hmac_fp);
-		fclose(vmlinux_fp);
-		return -1;
-	}
-
-	if (fseek(vmlinux_fp, offset, SEEK_SET) != 0) {
-		printf("Unable to seek into vmlinux file.");
-		fclose(hmac_fp);
-		fclose(vmlinux_fp);
-		return -1;
-	}
-
-	if (SHA256_DIGEST_SIZE !=  fwrite (hmac, sizeof(unsigned char), SHA256_DIGEST_SIZE, vmlinux_fp)) {
-		printf("Unable to write %d byte into vmlinux", SHA256_DIGEST_SIZE);
-		fclose(hmac_fp);
-		fclose(vmlinux_fp);
-		return -1;
-	}
-
-	fclose(vmlinux_fp);
-	fclose(hmac_fp);
-
-	return 0;
+	return -1;
 }
-
