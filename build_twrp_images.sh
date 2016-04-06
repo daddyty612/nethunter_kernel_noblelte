bc08d8294a7286decfd5e4115084a352af748fcf
James Christopher Adduono <jc@adduono.com>
2016-04-06 04:58:46 -0400

    A better, lazier, TWRP image builder

--- build_twrp_images.sh
+++ build_twrp_images.sh
@@ -15,37 +15,45 @@ OUT_DIR=/home/jc/build/twrp/device/samsung
 ARCH=arm64
 KDIR=$RDIR/build/arch/$ARCH/boot
 
+BUILD_DEVICE=$DEVICE
+
 MOVE_IMAGES()
 {
-	echo "Moving kernel Image and dtb.img to $VARIANT_DIR/..."
-	mkdir -p "$VARIANT_DIR"
-	rm -f "$VARIANT_DIR/Image" "$VARIANT_DIR/dtb.img"
-	mv "$KDIR/Image" "$KDIR/dtb.img" "$VARIANT_DIR/"
+	echo "Moving kernel Image and dtb.img to $1/..."
+	mkdir -p "$1"
+	rm -f "$1/Image" "$1/dtb.img"
+	mv "$KDIR/Image" "$KDIR/dtb.img" "$1/"
 }
 
-mkdir -p $OUT_DIR
-
-[ "$DEVICE" ] || DEVICE=noblelte
-[ "$1" ] && {
-	VARIANTS=$*
-} || {
-	VARIANTS=$(cat "$RDIR/VARIANTS")
+BUILD_VARIANTS()
+{
+	for VARIANT in $2
+	do
+		[ "$VARIANT" == "eur" ] && {
+			VARIANT_DIR=$OUT_DIR/$1
+		} || {
+			VARIANT_DIR=$OUT_DIR/$1$VARIANT
+		}
+		DEVICE="$1" "$RDIR/build.sh" "$VARIANT" && MOVE_IMAGES "$VARIANT_DIR" || {
+			echo "Failed to build $1-$VARIANT! Aborting..."
+			exit 1
+		}
+	done
 }
 
 export TARGET=twrp
-export DEVICE
 
-for VARIANT in $VARIANTS
-do
-	[ "$VARIANT" == "eur" ] && {
-		VARIANT_DIR=$OUT_DIR/$DEVICE
-	} || {
-		VARIANT_DIR=$OUT_DIR/$DEVICE$VARIANT
-	}
-	"$RDIR/build.sh" "$VARIANT" && MOVE_IMAGES || {
-		echo "Failed to build $DEVICE-$VARIANT! Aborting..."
-		exit 1
-	}
-done
+while read line; do
+	CURRENT_DEVICE=$(echo "$line" | awk -F'=' '{print $1}')
+	if [ "$BUILD_DEVICE" -a ! "$BUILD_DEVICE" = "$CURRENT_DEVICE" ]; then
+		continue
+	fi
+	if [ "$1" ]; then
+		VARIANTS=$1
+	else
+		VARIANTS=$(echo "$line" | awk -F'=' '{print $2}')
+	fi
+	BUILD_VARIANTS "$CURRENT_DEVICE" "$VARIANTS"
+done < "$RDIR/VARIANTS"
 
 echo "Finished building TWRP kernels!"
